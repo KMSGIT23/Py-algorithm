4032d5185771c450570f91eb9a4beeb793fe5d1f
KMS <128460782+KMSGIT23@users.noreply.github.com>
2023-09-19 13:13:20 +0900

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,8 +1,8 @@
-def f(n, f, e, a):
+def fa(n, f, e, a):
     if n==1:
         print(f, "->", e)
         return
-    f(n-1, f, a, e)
+    fa(n-1, f, a, e)
     print(f,"->", e)
-    f(n-1, a, e, f)
+    fa(n-1, a, e, f)
 
